eab6d00c022f4610475013a44c8f57baff0f5ccc
antonponmath <48967349+antonponmath@users.noreply.github.com>
2026-08-12 18:21:25 +0200

    plotting script parameters

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -1,3 +1,5 @@
+from math import pi, sqrt
+
 from matplotlib import pyplot as plt
 
 from pll_portrait.models import SRFPLL, LinearPendulum
@@ -6,10 +8,15 @@ from pll_portrait.simulation import simulate
 
 def main():
     # system = LinearPendulum(stiffness=1, damping=1)
+
     system = SRFPLL(
-        kp=1, ki=1000, frequency=314, positive_sequence_amplitude=230, unbalance_factor=0.1
+        kp=1,
+        ki=1000,
+        frequency=2 * pi * 50,
+        positive_sequence_amplitude=200,
+        unbalance_factor=0.1,
     )
-    result = simulate(system, tmax=100, initial_state=[0, 1])
+    result = simulate(system, tmax=30, initial_state=[0, 1])
 
     _, ax = plt.subplots()
     ax.plot(result.states[0, :], result.states[1, :], color="black", linewidth=2)
