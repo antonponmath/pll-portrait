aa9614166bfdea8c5dd7285722aaf525be3aa8af
antonponmath <48967349+antonponmath@users.noreply.github.com>
2026-08-13 19:16:47 +0200

    Squashed commit of the following:
    commit 5e5c1a2a173baf350149367a00947fd787f6ade4
    Author: antonponmath <48967349+antonponmath@users.noreply.github.com>
    Date:   Thu Aug 13 19:15:28 2026 +0200
    
        renaming, removed right comparison system from plotting script as it doesn't work in forward time
    
    commit e1f60631a48fdd30c2a0402ccc1a4679ac3cbf5e
    Author: antonponmath <48967349+antonponmath@users.noreply.github.com>
    Date:   Thu Aug 13 18:38:49 2026 +0200
    
        defined comparison systems for SRF-PLL
    
    commit cc63e6152467a1634c8f2ef69c5588aad329104c
    Author: antonponmath <48967349+antonponmath@users.noreply.github.com>
    Date:   Thu Aug 13 18:33:48 2026 +0200
    
        simulation outputs fine trajectory and cycle
    
    commit 0ff94e3c7adc3f6e7dc2a7c5e8b46161c8bad28c
    Author: antonponmath <48967349+antonponmath@users.noreply.github.com>
    Date:   Thu Aug 13 15:59:45 2026 +0200
    
        terminating on escape

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -3,33 +3,29 @@ from math import pi
 import numpy as np
 from matplotlib import pyplot as plt
 
-from pll_portrait.models import SRFPLL, LinearPendulum, VanDerPol
+from pll_portrait.models import ForcedSRFPLL, LinearPendulum, VanDerPol, ComparisonSRFPLL
 from pll_portrait.simulation import simulate
 
 
 def main():
     # system = LinearPendulum()
-    system = VanDerPol()
-    # system = SRFPLL()
+    # system = VanDerPol()
+    # system = ForcedSRFPLL()
+    system = ComparisonSRFPLL(left_or_right="right")
 
-    t_max = 20.0
-    initial_state = [0.0, 1.0]
-    result = simulate(system, t_final=t_max, initial_state=initial_state)
-    t_final = result.solution.t[-1]
-
-    traj_times = np.arange(0.0, t_final + system.max_step, system.max_step)
-    traj_states = result.solution.sol(traj_times)
+    t_max = 10
+    initial_state = [0.0, 0.1]
+    result = simulate(system, t_max=t_max, initial_state=initial_state)
 
     _, ax = plt.subplots()
-    ax.plot(traj_states[0, :], traj_states[1, :], color="black", linewidth=1)
-
-    if result.cycle_times is not None:
-        t1, t2 = result.cycle_times
-        cycle_times = np.arange(t1, t2 + system.max_step, system.max_step)
-        cycle_states = result.solution.sol(cycle_times)
-        ax.plot(cycle_states[0, :], cycle_states[1, :], color="red", linewidth=2)
+    ax.plot(
+        result.trajectory[0, :], result.trajectory[1, :], color="blue", linewidth=1
+    )
+    # if result.cycle is not None:
+    #     ax.plot(result.cycle[0, :], result.cycle[1, :], color="red", linewidth=2)
 
     ax.plot(initial_state[0], initial_state[1], marker="o", color="black")
+    ax.grid()
     plt.show()
 
 
