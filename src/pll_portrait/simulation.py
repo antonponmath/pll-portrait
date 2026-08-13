cc63e6152467a1634c8f2ef69c5588aad329104c
antonponmath <48967349+antonponmath@users.noreply.github.com>
2026-08-13 18:33:48 +0200

    simulation outputs fine trajectory and cycle

--- src/pll_portrait/simulation.py
+++ src/pll_portrait/simulation.py
@@ -1,15 +1,17 @@
 from math import pi
 
+import numpy as np
 from numpy import linalg as LA
 from scipy.integrate import solve_ivp
 
 
 class SimulationResult:
-    __slots__ = ("solution", "cycle_times")
+    __slots__ = ("solution", "trajectory", "cycle")
 
-    def __init__(self, *, solution, cycle_times):
+    def __init__(self, *, solution, trajectory, cycle):
         self.solution = solution
-        self.cycle_times = cycle_times
+        self.trajectory = trajectory
+        self.cycle = cycle
 
 
 def crossing_upward(_t, y):
@@ -21,7 +23,7 @@ crossing_upward.direction = +1
 
 def simulate(
     system,
-    t_final,
+    t_max,
     initial_state,
     cycle_tolerance=0.01,
     escape_bounds=(-pi, pi),
@@ -48,27 +50,38 @@ def simulate(
 
     solution = solve_ivp(
         fun=system,
-        t_span=(0.0, t_final + system.max_step),
+        t_span=(0.0, t_max),
         y0=initial_state,
         events=events,
         dense_output=True,
     )
 
-    cycle_times = None
-    if is_forced:
+    # TODO adapt to inverse time integration
+    t_final = solution.t[-1]
+    trajectory_times = np.append(np.arange(0.0, t_final, system.max_step), t_final)
+    trajectory = solution.sol(trajectory_times)
+
+    cycle = None
+    if escape_bounds is not None and (
+        len(solution.t_events[-2]) > 0 or len(solution.t_events[-1]) > 0
+    ):
+        # escaped
+        cycle = None
+    elif is_forced:
+        # check if there is a cycle at forcing period
         t1 = t_final - system.forcing_period
         t2 = t_final
         y1 = solution.sol(t1)
         y2 = solution.sol(t2)
         if LA.norm(y1 - y2) < cycle_tolerance:
-            cycle_times = (t1, t2)
-    else:
-        crossing_times = solution.t_events[0]
-        crossing_states = solution.y_events[0]
-        if (
-            len(crossing_times) > 1
-            and abs(crossing_states[-1, 0] - crossing_states[-2, 0]) < cycle_tolerance
-        ):
-            cycle_times = crossing_times[-2:]
+            cycle_times = np.append(np.arange(t1, t2, system.max_step), t2)
+            cycle = solution.sol(cycle_times)
+    elif cycle_tolerance is not None:
+        # check if there is a cycle between crossings
+        t = solution.t_events[0]
+        y = solution.y_events[0]
+        if len(t) > 1 and abs(y[-1, 0] - y[-2, 0]) < cycle_tolerance:
+            cycle_times = np.append(np.arange(t[-2], t[-1], system.max_step), t[-1])
+            cycle = solution.sol(cycle_times)
 
-    return SimulationResult(solution=solution, cycle_times=cycle_times)
+    return SimulationResult(solution=solution, trajectory=trajectory, cycle=cycle)
