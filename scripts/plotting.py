1277809002a9fb2062d8321e968aba9cf197106f
antonponmath <48967349+antonponmath@users.noreply.github.com>
2026-09-22 13:22:33 +0200

    removed unnecessary variables dotted style for inactive trajectory

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -16,69 +16,75 @@ def main():
     # system = LinearPendulum()
     # system = VanDerPol()
     # system = ForcedSRFPLL()
-    system_left = ComparisonSRFPLL(left_or_right="left", unbalance_factor=0.15)
-    system_right = ComparisonSRFPLL(left_or_right="right", unbalance_factor=0.15)
+    comparison_left = ComparisonSRFPLL(left_or_right="left", unbalance_factor=0.15)
+    comparison_right = ComparisonSRFPLL(left_or_right="right", unbalance_factor=0.15)
 
-    t_max = 30
+    t_max = 50
 
-    initial_state = [0.0, system_left.z_plus + 0.01]
-    result_cycle = simulate(system_left, t_max=t_max, initial_state=initial_state)
-
-    initial_state = [pi - 0.01, system_left.z_minus]
-    result_lockin_upper = simulate(
-        system_left, t_max=-t_max, initial_state=initial_state
+    exclusion_upper = simulate(
+        comparison_right,
+        t_max=-t_max,
+        initial_state=[pi - 0.01, comparison_left.z_plus + 0.01],
     )
-
-    initial_state = [-pi + 0.01, system_left.z_plus]
-    result_lockin_lower = simulate(
-        system_left, t_max=-t_max, initial_state=initial_state
+    exclusion_lower = simulate(
+        comparison_right,
+        t_max=-t_max,
+        initial_state=[-pi + 0.01, comparison_left.z_minus - 0.01],
     )
-
-    initial_state = [pi - 0.01, system_left.z_plus]
-    result_exclusion_upper = simulate(
-        system_right, t_max=-t_max, initial_state=initial_state
+    lockin_upper = simulate(
+        comparison_left,
+        t_max=-t_max,
+        initial_state=[pi - 0.01, comparison_left.z_minus + 0.01],
     )
-
-    initial_state = [-pi + 0.01, system_left.z_minus]
-    result_exclusion_lower = simulate(
-        system_right, t_max=-t_max, initial_state=initial_state
+    lockin_lower = simulate(
+        comparison_left,
+        t_max=-t_max,
+        initial_state=[-pi + 0.01, comparison_left.z_plus - 0.01],
+    )
+    estimation = simulate(
+        comparison_left,
+        t_max=t_max,
+        initial_state=[-pi + 0.01, lockin_lower.trajectory[1, -1] / 2.0]
+        if lockin_lower.trajectory[1, -1] > 0
+        else [-pi + 0.01, lockin_upper.trajectory[1, -1] / 2.0],
     )
 
     _, ax = plt.subplots()
     ax.plot(
-        result_lockin_upper.trajectory[0, :],
-        result_lockin_upper.trajectory[1, :],
-        color="blue",
-        linewidth=2,
-    )
-    ax.plot(
-        result_lockin_lower.trajectory[0, :],
-        result_lockin_lower.trajectory[1, :],
-        color="blue",
-        linewidth=2,
-    )
-    ax.plot(
-        result_exclusion_upper.trajectory[0, :],
-        result_exclusion_upper.trajectory[1, :],
+        exclusion_upper.trajectory[0, :],
+        exclusion_upper.trajectory[1, :],
         color="red",
         linewidth=2,
     )
     ax.plot(
-        result_exclusion_lower.trajectory[0, :],
-        result_exclusion_lower.trajectory[1, :],
+        exclusion_lower.trajectory[0, :],
+        exclusion_lower.trajectory[1, :],
         color="red",
         linewidth=2,
     )
     ax.plot(
-        result_cycle.trajectory[0, :],
-        result_cycle.trajectory[1, :],
+        lockin_upper.trajectory[0, :],
+        lockin_upper.trajectory[1, :],
+        color="blue",
+        linewidth=2,
+        linestyle=":" if lockin_lower.trajectory[0, -1] < 0 else "-",
+    )
+    ax.plot(
+        lockin_lower.trajectory[0, :],
+        lockin_lower.trajectory[1, :],
+        color="blue",
+        linewidth=2,
+    )
+    ax.plot(
+        estimation.trajectory[0, :],
+        estimation.trajectory[1, :],
         color="gray",
         linewidth=1,
     )
-    if result_cycle.cycle is not None:
+    if estimation.cycle is not None:
         ax.plot(
-            result_cycle.cycle[0, :],
-            result_cycle.cycle[1, :],
+            estimation.cycle[0, :],
+            estimation.cycle[1, :],
             color="green",
             linewidth=2,
         )
